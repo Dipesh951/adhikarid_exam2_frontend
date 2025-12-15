5238ea73a96ad264dd7b47b04546caa81e7e143a
Dipesh Adhikari <adhikarid@uwplatt.edu>
2025-12-15 13:01:39 -0600

    some changes in api.js

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 const API =
-  window.location.hostname === "localhost"
-    ? "http://localhost:8081"
-    : "https://adhikari-exam2-backend.onrender.com";
+//   window.location.hostname === "localhost"
+    // ? "http://localhost:8081"
+     "https://adhikari-exam2-backend.onrender.com";
 
 export default API;
